911a5a92f8b8a986c50af2990ee7a9cc8cde009c
Pr0vjp <lebathang7b@gmail.com>
2023-11-21 00:45:03 +0700

    next commit

--- js/readme.js
+++ js/readme.js
@@ -54,3 +54,9 @@ window.onload = function() {
     // css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     // document.body.appendChild(css);
 };
+
+$(document).ready(function() {
+    $('#logo').click(function() {
+        $('nav').slideToggle();
+    });
+})
